5f0eb68a20ab2fdb577c506a3b76cedfc34f37ba
Brandon <Brandonstowe71@gmail.com>
2021-08-17 08:38:12 -0500

    added axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 
@@ -16,6 +18,18 @@ export class MainView extends React.Component {
     }
   }
 
+  componentDidMount(){
+    axios.get('https://myflixdbcfv3.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
@@ -25,7 +39,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
       <div className="main-view">
